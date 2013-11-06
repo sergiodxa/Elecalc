19fe67bace8441b1af6e91487aabcbe7e2124720
sergiodxa <sergio@xalambri.com.ar>
2013-11-05 21:22:34 -0300

    change to vanilla.js

--- js/main.js
+++ js/main.js
@@ -1,79 +1,79 @@
 function resCalc () {
-	var value1 = parseFloat($('#res1_sumRes').val()),
-		value2 = parseFloat($('#res2_sumRes').val()),
-		type = $("#type_sumRes input[name='type']:checked").val(); 
+	var value1 = parseFloat(document.querySelector('#res1_sumRes').value),
+		value2 = parseFloat(document.querySelector('#res2_sumRes').value),
+		type = document.querySelector("#type_sumRes input[name='type']:checked").value; 
 	if (type=="Serie") {
 		res = value1+value2;
-		$('#result_sumRes').attr('value', res+'Ω‎');
+		document.querySelector('#result_sumRes').value = res+'Ω';
 	}
 	else if (type=="Parallel") {
 		multi = value1*value2;
 		sum = value1+value2;
 		res = multi/sum;
-		$('#result_sumRes').attr('value', res+'Ω‎');
+		document.querySelector('#result_sumRes').value = res+'Ω';
 	}
 	else {
-		$('#result_sumRes').attr('value', 'How the hell did you not select anything?');
+		document.querySelector('#result_sumRes').value = res+'How the hell did you not select anything?';
 	}
 };
 function ohmCalc () {
-	var voltage = parseFloat($('#voltage_ohmCalc').val()),
-		intensity = parseFloat($('#intensity_ohmCalc').val()),
-		resistance = parseFloat($('#resistance_ohmCalc').val());
+	var voltage = parseFloat(document.querySelector('#voltage_ohmCalc').value),
+		intensity = parseFloat(document.querySelector('#intensity_ohmCalc').value),
+		resistance = parseFloat(document.querySelector('#resistance_ohmCalc').value);
 	if (intensity>0 && resistance>0 && isNaN(voltage)) {
 		voltage = intensity*resistance;
-		$('#voltage_ohmCalc').attr('value', voltage+'V');
+		document.querySelector('#voltage_ohmCalc').value = voltage+'V';
 	}
 	else if (voltage>0 && resistance>0 && isNaN(intensity)) {
 		intensity = voltage/resistance;
-		$('#intensity_ohmCalc').attr('value', intensity+'A');		
+		document.querySelector('#intensity_ohmCalc').value = intensity+'A';
 	}
 	else if (voltage>0 && intensity>0 && isNaN(resistance)) {
 		resistance = voltage/intensity;
-		$('#resistance_ohmCalc').attr('value', resistance+'Ω');		
+		document.querySelector('#resistance_ohmCalc').value = resistance+'Ω';
 	}
 	else {
 		alert('Error, please let only one empty');
 	}
 };
 function powerCalc () {
-	var voltage = parseFloat($('#voltage_powerCalc').val()),
-		intensity = parseFloat($('#intensity_powerCalc').val()),
-		cosPhi = parseFloat($('#cosPhi_powerCalc').val()),
-		power = parseFloat($('#power_powerCalc').val());
+	var voltage = parseFloat(document.querySelector('#voltage_powerCalc').value),
+		intensity = parseFloat(document.querySelector('#intensity_powerCalc').value),
+		cosPhi = parseFloat(document.querySelector('#cosPhi_powerCalc').value),
+		power = parseFloat(document.querySelector('#power_powerCalc').value);
 	if (intensity>0 && cosPhi>0 && power>0 && isNaN(voltage)) {
 		voltage = power/(intensity*cosPhi);
-		$('#voltage_powerCalc').attr('value', voltage+'V');
+		document.querySelector('#voltage_powerCalc').value = voltage+'V';
 	}
 	else if (voltage>0 && cosPhi>0 && power>0 && isNaN(intensity)) {
 		intensity = power/(voltage*cosPhi);
-		$('#intensity_powerCalc').attr('value', intensity+'A');
+		document.querySelector('#intensity_powerCalc').value = intensity+'A';
 	}
 	else if (voltage>0 && intensity>0 && power>0 && isNaN(cosPhi)) {
 		cosPhi = power/(voltage*intensity);
-		$('#cosPhi_powerCalc').attr('value', cosPhi);
+		document.querySelector('#cosPhi_powerCalc').value = cosPhi;
 	}
 	else if (voltage>0 && cosPhi>0 && intensity>0 && isNaN(power)) {
 		power = voltage*intensity*cosPhi;
-		$('#power_powerCalc').attr('value', power+'W');
+		document.querySelector('#power_powerCalc').value = power+'W';
 	}
 	else {
 		alert('Error, please let only one empty');
 	}
 };
 function maxwellCalc () {
-	var c1r1 = parseFloat($('#col1row1_maxwellCalc').val()),
-		c2r1 = parseFloat($('#col2row1_maxwellCalc').val()),
-		c3r1 = parseFloat($('#col3row1_maxwellCalc').val()),
-		c1r2 = parseFloat($('#col1row2_maxwellCalc').val()),
-		c2r2 = parseFloat($('#col2row2_maxwellCalc').val()),
-		c3r2 = parseFloat($('#col3row2_maxwellCalc').val()),
-		c1r3 = parseFloat($('#col1row3_maxwellCalc').val()),
-		c2r3 = parseFloat($('#col2row3_maxwellCalc').val()),
-		c3r3 = parseFloat($('#col3row3_maxwellCalc').val()),
-		value1 = parseFloat($('#substituteValue1_maxwellCalc').val()),
-		value2 = parseFloat($('#substituteValue2_maxwellCalc').val()),
-		value3 = parseFloat($('#substituteValue3_maxwellCalc').val());
+	var c1r1 = parseFloat(document.querySelector('#col1row1_maxwellCalc').value),
+		c2r1 = parseFloat(document.querySelector('#col2row1_maxwellCalc').value),
+		c3r1 = parseFloat(document.querySelector('#col3row1_maxwellCalc').value),
+		c1r2 = parseFloat(document.querySelector('#col1row2_maxwellCalc').value),
+		c2r2 = parseFloat(document.querySelector('#col2row2_maxwellCalc').value),
+		c3r2 = parseFloat(document.querySelector('#col3row2_maxwellCalc').value),
+		c1r3 = parseFloat(document.querySelector('#col1row3_maxwellCalc').value),
+		c2r3 = parseFloat(document.querySelector('#col2row3_maxwellCalc').value),
+		c3r3 = parseFloat(document.querySelector('#col3row3_maxwellCalc').value),
+		value1 = parseFloat(document.querySelector('#substituteValue1_maxwellCalc').value),
+		value2 = parseFloat(document.querySelector('#substituteValue2_maxwellCalc').value),
+		value3 = parseFloat(document.querySelector('#substituteValue3_maxwellCalc').value);
 	base = ((c1r1*c2r2*c3r3)+(c1r2*c2r3*c3r1)+(c1r3*c2r1*c3r2))-((c3r1*c2r2*c1r3)+(c3r2*c2r3*c1r1)+(c3r3*c2r1*c1r2));
 	sust1 = ((value1*c2r2*c3r3)+(value2*c2r3*c3r1)+(value3*c2r1*c3r2))-((c3r1*c2r2*value3)+(c3r2*c2r3*value1)+(c3r3*c2r1*value2));
 	sust2 = ((c1r1*value2*c3r3)+(c1r2*value3*c3r1)+(c1r3*value1*c3r2))-((c3r1*value2*c1r3)+(c3r2*value3*c1r1)+(c3r3*value1*c1r2));
@@ -82,66 +82,66 @@ function maxwellCalc () {
 	int2 = sust2/base;
 	int3 = sust3/base;
 	if (isNaN(int1) && isNaN(int2) && isNaN(int3)) {
-		$('#result1_maxwellCalc').attr('value', 'Error, in this equation I need to divide by 0');
-		$('#result2_maxwellCalc').attr('value', 'Error, in this equation I need to divide by 0');
-		$('#result3_maxwellCalc').attr('value', 'Error, in this equation I need to divide by 0');
+		document.querySelector('#result1_maxwellCalc').value = 'Error, in this equation I need to divide by 0';
+		document.querySelector('#result2_maxwellCalc').value = 'Error, in this equation I need to divide by 0';
+		document.querySelector('#result3_maxwellCalc').value = 'Error, in this equation I need to divide by 0';
 	}
 	else {
-		$('#result1_maxwellCalc').attr('value', int1+'A');
-		$('#result2_maxwellCalc').attr('value', int2+'A');
-		$('#result3_maxwellCalc').attr('value', int3+'A');
+		document.querySelector('#result1_maxwellCalc').value = int1+'A';
+		document.querySelector('#result2_maxwellCalc').value = int2+'A';
+		document.querySelector('#result3_maxwellCalc').value = int3+'A';
 	}
 };
 function lumenCalc () {
-	var averageIluminance = parseFloat($('#averageIluminance_lumenCalc').val()),
-		workPlane = parseFloat($('#workPlane_lumenCalc').val()),
-		maintainceFactor = parseFloat($('#maintainceFactor_lumenCalc').val()),
-		utilizationFactor = parseFloat($('#utilizationFactor_lumenCalc').val()),
-		luminousFlux = parseFloat($('#luminousFlux_lumenCalc').val());
+	var averageIluminance = parseFloat(document.querySelector('#averageIluminance_lumenCalc').value),
+		workPlane = parseFloat(document.querySelector('#workPlane_lumenCalc').value),
+		maintainceFactor = parseFloat(document.querySelector('#maintainceFactor_lumenCalc').value),
+		utilizationFactor = parseFloat(document.querySelector('#utilizationFactor_lumenCalc').value),
+		luminousFlux = parseFloat(document.querySelector('#luminousFlux_lumenCalc').value);
 	if (averageIluminance>0 && workPlane>0 && maintainceFactor>0 && utilizationFactor>0 && isNaN(luminousFlux)) {
 		luminousFlux = (averageIluminance*workPlane)/(maintainceFactor*utilizationFactor);
-		$('#luminousFlux_lumenCalc').attr('value', luminousFlux);
+		document.querySelector('#luminousFlux_lumenCalc').value = luminousFlux;
 	}
 	else if (luminousFlux>0 && workPlane>0 && maintainceFactor>0 && utilizationFactor>0 && isNaN(averageIluminance)) {
 		averageIluminance = (luminousFlux*maintainceFactor*utilizationFactor)/(workPlane);
-		$('#averageIluminance_lumenCalc').attr('value', averageIluminance);
+		document.querySelector('#averageIluminance_lumenCalc').value = averageIluminance;
 	}
 	else if (averageIluminance>0 && luminousFlux>0 && maintainceFactor>0 && utilizationFactor>0 && isNaN(workPlane)) {
 		workPlane = (luminousFlux*maintainceFactor*utilizationFactor)/(averageIluminance);
-		$('#workPlane_lumenCalc').attr('value', workPlane);
+		document.querySelector('#workPlane_lumenCalc').value = workPlane;
 	}
 	else if (averageIluminance>0 && workPlane>0 && luminousFlux>0 && utilizationFactor>0 && isNaN(maintainceFactor)) {
 		maintainceFactor = (averageIluminance*workPlane)/(luminousFlux*utilizationFactor);
-		$('#maintainceFactor_lumenCalc').attr('value', maintainceFactor);
+		document.querySelector('#maintainceFactor_lumenCalc').value = maintainceFactor;
 	}
 	else if (averageIluminance>0 && workPlane>0 && maintainceFactor>0 && luminousFlux>0 && isNaN(utilizationFactor)) {
 		utilizationFactor = (averageIluminance*workPlane)/(maintainceFactor*luminousFlux);
-		$('#utilizationFactor_lumenCalc').attr('value', utilizationFactor);
+		document.querySelector('#utilizationFactor_lumenCalc').value = utilizationFactor;
 	}
 	else {
 		alert('Error, please let only one empty');
 	}
 };
 function luminairesCalc () {
-	var luminousFlux = parseFloat($('#luminousFlux_luminairesCalc').val()),
-		flowLamp = parseFloat($('#flowLamp_luminairesCalc').val()),
-		lampsFixture = parseFloat($('#lampsFixture_luminairesCalc').val()),
-		luminaires = parseFloat($('#luminaires_luminairesCalc').val());
+	var luminousFlux = parseFloat(document.querySelector('#luminousFlux_luminairesCalc').value),
+		flowLamp = parseFloat(document.querySelector('#flowLamp_luminairesCalc').value),
+		lampsFixture = parseFloat(document.querySelector('#lampsFixture_luminairesCalc').value),
+		luminaires = parseFloat(document.querySelector('#luminaires_luminairesCalc').value);
 	if (luminousFlux>0 && flowLamp>0 && lampsFixture>0 && isNaN(luminaires)) {
 		luminaires = luminousFlux/(flowLamp*lampsFixture);
-		$('#luminaires_luminairesCalc').attr('value', luminaires);
+		document.querySelector('#luminaires_luminairesCalc').value = luminaires;
 	}
 	else if (luminaires>0 && flowLamp>0 && lampsFixture>0 && isNaN(luminousFlux)) {
 		luminousFlux = luminaires*flowLamp*lampsFixture;
-		$('#luminousFlux_luminairesCalc').attr('value', luminousFlux);
+		document.querySelector('#luminousFlux_luminairesCalc').value = luminousFlux;
 	}
 	else if (luminaires>0 && luminousFlux>0 && lampsFixture>0 && isNaN(flowLamp)) {
 		flowLamp = luminousFlux/(luminaires*lampsFixture);
-		$('#flowLamp_luminairesCalc').attr('value', flowLamp);
+		document.querySelector('#flowLamp_luminairesCalc').value = flowLamp;
 	}
 	else if (luminaires>0 && flowLamp>0 && luminousFlux>0 && isNaN(lampsFixture)) {
 		lampsFixture = luminousFlux/(flowLamp*luminaires);
-		$('#lampsFixture_luminairesCalc').attr('value', lampsFixture);
+		document.querySelector('#lampsFixture_luminairesCalc').value = lampsFixture;
 	}
 	else {
 		alert('Error, please let only one empty');
